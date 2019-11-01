73d59e1fb0796f58c2c07ae6191726a2d8c45225
Angie <arco527@gmail.com>
2019-10-31 21:43:41 -0500

    Second commit

--- src/App.js
+++ src/App.js
@@ -5,23 +5,14 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+      <div>
         <h1>
         Mi primera aplicación
         </h1>
         <p>
         Esta es mi primera aplicación en React, está padrísimo!
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      </div>
     </div>
   );
 }
